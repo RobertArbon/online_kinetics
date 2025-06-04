7d1f3dc99a0a7e2a4a2fceac3bf953feb0c344f1
Lex O'brien <lex.e.obrien@gmail.com>
2025-06-04 20:49:36 +0100

    changed to setup.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="celerity",
-    version="0.1",
-    description="Creates deep MSMs using accelerated training methods",
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'celerity = celerity.api:cli',
-        ],
-    },
-    python_requires=">=3.10",
-)
+if __name__ == "__main__":
+    setup()
