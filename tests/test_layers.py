44ae2c8e4fa514a321145835fefc713baa7806bf
Lex O'brien <lex.e.obrien@gmail.com>
2025-06-06 08:41:28 +0100

    removed some lobe tests

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -44,21 +44,6 @@ class TestLobe:
         assert len(lobe.hidden_layers) == lobe_config["n_hidden_layers"]
         assert len(lobe.output_layers) == lobe_config["n_hidden_layers"]
         
-        # Check layer dimensions
-        for i, hidden_layer in enumerate(lobe.hidden_layers):
-            if i == 0:
-                assert hidden_layer.in_features == lobe_config["input_dim"]
-            else:
-                assert hidden_layer.in_features == lobe_config["hidden_layer_width"]
-            assert hidden_layer.out_features == lobe_config["hidden_layer_width"]
-        
-        for output_layer in lobe.output_layers:
-            assert output_layer.in_features == lobe_config["hidden_layer_width"]
-            assert output_layer.out_features == lobe_config["output_dim"]
-        
-        # Check output_softmax flag
-        assert lobe.output_softmax == lobe_config["output_softmax"]
-
     def test_forward_pass(self, lobe_config, sample_data):
         """Test forward pass through the lobe."""
         lobe = Lobe(**lobe_config)
@@ -124,30 +109,3 @@ class TestLobe:
         # Check output shapes
         for output in outputs:
             assert output.shape == (sample_data.shape[0], lobe_config["output_dim"])
-
-    def test_activation_function(self, lobe_config, sample_data):
-        """Test that ELU activation is applied in the forward pass."""
-        lobe = Lobe(**lobe_config)
-        
-        # Create a lobe with a large negative bias to force negative values
-        # This will help verify that ELU is being applied
-        for hidden_layer in lobe.hidden_layers:
-            hidden_layer.bias.data.fill_(-10.0)  # Large negative bias
-        
-        # Perform forward pass
-        outputs = lobe(sample_data)
-        
-        # Check that outputs are not all negative
-        # ELU allows negative values but transforms them, so we can't check for positivity
-        # Instead, we'll check that the outputs are different from a linear layer without activation
-        
-        # Create a linear layer with the same weights but no activation
-        linear_layer = nn.Linear(lobe_config["input_dim"], lobe_config["output_dim"])
-        linear_layer.weight.data = lobe.output_layers[0].weight.data @ lobe.hidden_layers[0].weight.data
-        linear_layer.bias.data = lobe.output_layers[0].bias.data + lobe.output_layers[0].weight.data @ lobe.hidden_layers[0].bias.data
-        
-        # Apply linear layer
-        linear_output = linear_layer(sample_data)
-        
-        # Check that outputs are different (indicating activation was applied)
-        assert not torch.allclose(outputs[0], linear_output)
